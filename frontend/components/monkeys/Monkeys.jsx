9a3c0b279e75463bc93aae4e6e09134515e4d9b8
suhai yehuza <soyehuza@gmail.com>
2017-07-23 12:04:24 -0700

    added sample video for testing videos

--- frontend/components/monkeys/Monkeys.jsx
+++ frontend/components/monkeys/Monkeys.jsx
@@ -2,16 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navigationa from '../navigationa';
 
-const Monkeys = () => (
-  <div className="app">
-    <header className='loggedhome-header'>
-      <Navigationa />
-    </header>
-    <h2 className='tempora'>This is the Monkeys Component</h2>
-    <h3 className='tempora'>Sign Up or LogIn for Exclusive access to Content</h3>
-    <h1 className='tempora'>Content Coming Soon</h1>
-    
-  </div>
-);
+class Monkeys extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      videoURL: 'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4'
+    }
+  }
+
+  render () {
+    return (
+      <div className="app">
+        <header className='loggedhome-header'>
+          <Navigationa />
+        </header>
+        <h2 className='tempora'>This is the Monkeys Component</h2>
+        <h3 className='tempora'>Sign Up or LogIn for Exclusive access to Content</h3>
+        <h1 className='tempora'>Content Coming Soon</h1> 
+        <video id="background-video" loop autoPlay>
+            <source src={this.state.videoURL} type="video/mp4" />
+            <source src={this.state.videoURL} type="video/ogg" />
+            Your browser does not support the video tag.
+        </video>
+      </div>
+    )  
+  }
+}
 
 export default Monkeys;
