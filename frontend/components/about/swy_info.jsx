0066fac0d88e34f8fabe78e6802799f23765bd0f
suhai yehuza <soyehuza@gmail.com>
2017-07-24 14:54:05 -0700

    reinstalled bundler

--- frontend/components/about/swy_info.jsx
+++ frontend/components/about/swy_info.jsx
@@ -7,7 +7,7 @@ const SwyInfo = () => (
     <header className='loggedhome-header'>
       <Navigationa />
     </header>
-   
+      {/* <img src="https://drive.google.com/open?id=0B3YmJk3--C3rLW8xcVI0YTQweTQ" alt="swy_image"/> */}
     <h3 className='temporal'>About Me</h3>
     <div>
       <p className='about-me'>     
