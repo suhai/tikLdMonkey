6dc986ec42971b96d2bb56fa719a07b1e97b9a30
suhai yehuza <soyehuza@gmail.com>
2017-07-25 14:58:33 -0700

    diabled delete functionality

--- frontend/components/categories/Category.jsx
+++ frontend/components/categories/Category.jsx
@@ -4,24 +4,20 @@ import ReactDOM from 'react-dom';
 class Category extends React.Component {
   constructor(props) {
     super(props);
-    // this.deleteCategory = this.deleteCategory.bind(this);
     
     this.state = {
 
     }
   }
 
-  deleteCategory(id) {
-    // this.props.deleteCategory(id);
-  }
 
   render() {
     return (
       <li className='user-page-options list'>
         <a href="#">{this.props.category.name}</a>
         <aside>
-          <button onClick={this.props.editCategory}className='user-page-edit'>Edit</button>
-          <button onClick={this.props.deleteCategory} className='user-page-delete'>Delete</button>
+          <button className='user-page-edit'>Edit</button>
+          <button className='user-page-delete'>Delete</button>
         </aside>
       </li>
     );
