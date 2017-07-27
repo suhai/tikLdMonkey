4cfb6b234b4181a5014b1e136acab9646e8cbabf
suhai yehuza <soyehuza@gmail.com>
2017-07-27 10:20:32 -0700

    moved around the navbar content

--- frontend/components/navigationa.jsx
+++ frontend/components/navigationa.jsx
@@ -28,8 +28,8 @@ class Navigationa extends React.Component {
         <ul>
           <li className='loggedhome-logo'><img src='http://res.cloudinary.com/swy/image/upload/v1499673174/images/monkey.svg'/></li>
           <li><NavLink exact to='/'>Home</NavLink></li>
-          <li><NavLink to='/trivia'>Trivia</NavLink></li>
           <li><NavLink to='/projects'>Projects</NavLink></li>
+          <li><NavLink to='/trivia'>Trivia</NavLink></li>
           <li><NavLink to='/monkeys'>Monkeys</NavLink></li>
           <li><NavLink to='/games'>Games</NavLink></li>
           <li><NavLink to='/tech_news'>Tech News</NavLink></li>
